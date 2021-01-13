8346041f6eb34545bd95441f7e3fa9da0221600e
patrick quan <p.quan@me.com>
2021-01-12 17:33:02 -0700

    handle spaces in names

--- src/app.js
+++ src/app.js
@@ -1,51 +1,35 @@
-require("dotenv").config();
-const { isEmpty } = require("lodash");
-const { Client } = require("discord.js");
+require('dotenv').config();
+const { isEmpty } = require('lodash');
+const { Client } = require('discord.js');
 const client = new Client();
 
-const buildMessage = require("./buildMessage.js");
-const fetchData = require("./fetchData.js");
+const buildMessage = require('./buildMessage.js');
+const fetchData = require('./fetchData.js');
 
-const PREFIX = "!val";
+const PREFIX = '!val';
 
-client.on("ready", () => {
+client.on('ready', () => {
   console.log(`${client.user.tag} has logged in.`);
 });
 
-client.on("message", async (message) => {
-  if (
-    message.author.bot ||
-    !message.content.startsWith(PREFIX) ||
-    message.content.trim().length < 5
-  )
-    return;
+client.on('message', async (message) => {
+  if (message.author.bot || !message.content.startsWith(PREFIX) || message.content.trim().length < 5) return;
+  const username = encodeURI(message.content.trim().substring(PREFIX.length + 1));
 
-  const [command, ...usernames] = message.content
-    .trim()
-    .substring(PREFIX.length)
-    .match(/[^ ]+/g);
+  const data = await fetchData(username);
 
-  if (!isEmpty(usernames.length)) {
-    usernames.forEach(async (username) => {
-      const reply = buildMessage({ command, username });
-      message.channel.send(reply);
+  if (isEmpty(data)) {
+    const reply = buildMessage({
+      command: 'User Not Found',
+      username,
+      desc: `The user ${username} could not be found.`,
     });
-  } else {
-    const data = await fetchData(command);
-
-    if (isEmpty(data)) {
-      const reply = buildMessage({
-        command: "User Not Found",
-        username: command,
-        desc: `The user ${command} could not be found.`,
-      });
-      message.channel.send(reply);
-      return;
-    }
-
-    const reply = buildMessage({ command: "stats", data });
     message.channel.send(reply);
+    return;
   }
+
+  const reply = buildMessage({ command: 'stats', data });
+  message.channel.send(reply);
 });
 
 client.login(process.env.DISCORDJS_BOT_TOKEN);
